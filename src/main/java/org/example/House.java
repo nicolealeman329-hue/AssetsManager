6bc98cb668b170d1b2c75f1b22e696ee546a6a5e
nicolecarranza <nicolealeman329@gmail.com>
2026-05-12 15:08:18 -0400

    commit

--- src/main/java/org/example/House.java
+++ src/main/java/org/example/House.java
@@ -2,7 +2,66 @@ package org.example;
 
 public class House extends Asset {
 
-    private Sting address;
+    private String address;
     private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String decscription, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(decscription, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+@Override
+    public double getValue(){
+        double rate;
+
+        if(condition == 1 ){
+            rate = 180.0;
+        }else if (condition == 2){
+            rate = 130.0;
+        }else if (condition == 3){
+            rate = 90.0;
+        }else if(condition == 4 ){
+            rate = 80.0;
+        }else{
+            rate = 0.0;
+        }
+        return(squareFoot * rate)+(lotSize*0.25);
+}
 
 }
